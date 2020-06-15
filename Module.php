02931b04d0fd60b025d85edfe774cc40052192d3
Daniel Berthereau <Daniel.github@Berthereau.net>
2020-06-15 00:00:00 +0000

    Added a way to fill user settings via rest api.

--- Module.php
+++ Module.php
@@ -36,6 +36,23 @@ class Module extends AbstractModule
 
     public function attachListeners(SharedEventManagerInterface $sharedEventManager)
     {
+        // Manage user settings via rest api.
+        $sharedEventManager->attach(
+            \Omeka\Api\Adapter\UserAdapter::class,
+            'api.hydrate.pre',
+            [$this, 'apiHydratePreUser']
+        );
+        $sharedEventManager->attach(
+            \Omeka\Api\Adapter\UserAdapter::class,
+            'api.create.post',
+            [$this, 'apiCreateOrUpdatePostUser']
+        );
+        $sharedEventManager->attach(
+            \Omeka\Api\Adapter\UserAdapter::class,
+            'api.update.post',
+            [$this, 'apiCreateOrUpdatePostUser']
+        );
+
         // Add the user profile to the user show admin pages.
         $sharedEventManager->attach(
             'Omeka\Controller\Admin\User',
@@ -117,6 +134,8 @@ class Module extends AbstractModule
                 return;
             }
             $this->handleAnySettings($event, 'user_settings');
+        } elseif ($status->isApiRequest()) {
+            $this->handleAnySettings($event, 'user_settings');
         } else {
             parent::handleUserSettings($event);
         }
@@ -137,13 +156,18 @@ class Module extends AbstractModule
         $settings = $services->get('Omeka\Settings');
         $elements = $settings->get('userprofile_elements', '');
 
-        $userSettings = $services->get('Omeka\Settings\User');
+        $status = $services->get('Omeka\Status');
+        $userExist = !$status->isApiRequest() || $status->getRouteMatch()->getParam('id');
+        $userSettings = $userExist
+            ? $services->get('Omeka\Settings\User')
+            : null;
+
         if ($elements) {
             $iniReader = new IniReader;
             $ini = $iniReader->fromString($elements);
             if (!empty($ini['elements'])) {
                 foreach ($ini['elements'] as $name => $element) {
-                    $data[$name] = $userSettings->get($name);
+                    $data[$name] = $userExist ? $userSettings->get($name) : null;
                     $formFieldset
                         ->add($element)
                         ->get($name)->setValue($data[$name]);
@@ -171,6 +195,179 @@ class Module extends AbstractModule
         }
 
         return $form;
+    }
+
+    public function apiHydratePreUser(Event $event)
+    {
+        $services = $this->getServiceLocator();
+
+        // Only for the rest api manager.
+        /** @var \Omeka\Mvc\Status $status */
+        $status = $services->get('Omeka\Status');
+        if (!$status->isApiRequest()) {
+            return;
+        }
+
+        // TODO Manage "required" during create and update.
+
+        // Only if the request has settings.
+        /** @var \Omeka\Api\Request $request */
+        $request = $event->getParam('request');
+        $requestUserSettings = $request->getValue('o:setting');
+        if (!$requestUserSettings) {
+            return;
+        }
+
+        /** @var \Omeka\Stdlib\ErrorStore $errorStore */
+        $errorStore = $event->getParam('errorStore');
+        $user = $event->getParam('entity');
+        $fieldset = $this->userSettingsFieldset($user ? $user->getId() : null);
+
+        if (!is_array($requestUserSettings)) {
+            $errorStore->addError('o:setting', new Message(
+                'The key “o:setting” should be an array of user settings.' // @translate
+            ));
+            return;
+        }
+
+        foreach ($requestUserSettings as $key => $value) {
+            if (!$fieldset->has($key)) {
+                continue;
+            }
+
+            // TODO Use the validator of the element.
+            /** @var \Zend\Form\Element $element */
+            $element = $fieldset->get($key);
+            $isMultipleValues = is_array($value);
+
+            if ($element->getAttribute('required')
+                && (($isMultipleValues && !count($value)) || (!$isMultipleValues && !strlen($value)))
+            ) {
+                $errorStore->addError('o:setting', new Message(
+                    'A value is required for “%s”.', // @translate
+                    $key
+                ));
+                continue;
+            }
+
+            // Currently, only select and multicheckbox are checked.
+            if (method_exists($element, 'getValueOptions')) {
+                $valueOptions = $element->getValueOptions();
+                if ($isMultipleValues) {
+                    $values = array_intersect_key($valueOptions, array_flip($value));
+                    if (method_exists($element, 'isMultiple') && !$element->isMultiple()) {
+                        $errorStore->addError('o:setting', new Message(
+                            'Only one value is allowed for “%s”.', // @translate
+                            $key
+                        ));
+                    } elseif (count($value) !== count($values)) {
+                        $errorStore->addError('o:setting', new Message(
+                            'One or more values (“%s”) are not allowed for “%s”.', // @translate
+                            implode('”, “', array_diff($value, array_keys($valueOptions))), $key
+                        ));
+                    } elseif (!count($values) && $element->getAttribute('required')) {
+                        $errorStore->addError('o:setting', new Message(
+                            'A value is required for “%s”.', // @translate
+                            $key
+                        ));
+                    }
+                } else {
+                    if (strlen($value) && !isset($valueOptions[$value])) {
+                        $errorStore->addError('o:setting', new Message(
+                            'The value “%s” is not allowed for “%s”.', // @translate
+                            $value, $key
+                        ));
+                    }
+                }
+            } else {
+                if (method_exists($element, 'isMultiple') && $element->isMultiple()
+                    || $element instanceof \Zend\Form\Element\MultiCheckbox
+                ) {
+                    $errorStore->addError('o:setting', new Message(
+                        'An array of values is required for “%s”.', // @translate
+                        $key
+                    ));
+                } elseif (!isset($valueOptions[$value])) {
+                    $errorStore->addError('o:setting', new Message(
+                        'The value “%s” is not allowed for “%s”.', // @translate
+                        $value, $key
+                    ));
+                }
+            }
+            // TODO Add more check or use element validator.
+        }
+    }
+
+    public function apiCreateOrUpdatePostUser(Event $event)
+    {
+        $services = $this->getServiceLocator();
+
+        // Only for the rest api manager.
+        /** @var \Omeka\Mvc\Status $status */
+        $status = $services->get('Omeka\Status');
+        if (!$status->isApiRequest()) {
+            return;
+        }
+
+        // Only if the request has settings.
+        /** @var \Omeka\Api\Request $request */
+        $request = $event->getParam('request');
+        $requestUserSettings = $request->getValue('o:setting');
+        if (!$requestUserSettings || !is_array($requestUserSettings)) {
+            return;
+        }
+
+        /** @var \Omeka\Stdlib\ErrorStore $errorStore */
+        $user = $event->getParam('response')->getContent();
+        $userSettings = $services->get('Omeka\Settings\User');
+        $userSettings->setTargetId($user->getId());
+        $fieldset = $this->userSettingsFieldset($user->getId());
+
+        foreach ($requestUserSettings as $key => $value) {
+            // Silently skip if not exist for security and clean process.
+            if (!$fieldset->has($key)) {
+                continue;
+            }
+
+            // TODO Use the validator of the element.
+            /** @var \Zend\Form\Element $element */
+            $element = $fieldset->get($key);
+            $isMultipleValues = is_array($value);
+
+            // Some useless cleaning.
+            if (method_exists($element, 'getValueOptions') && $isMultipleValues) {
+                $value = array_keys(array_intersect_key($element->getValueOptions(), array_flip($value)));
+            } elseif ($element instanceof \Zend\Form\Element\Checkbox) {
+                $value = (bool) $value;
+            } elseif ($element instanceof \Zend\Form\Element\Number) {
+                $value = (int) $value;
+            }
+
+            $userSettings->set($key, $value);
+        }
+    }
+
+    /**
+     * Get the fieldset "user-setting" of the user form.
+     *
+     * @param int $userId
+     * @return \Zend\Form\Fieldset
+     */
+    protected function userSettingsFieldset($userId = null)
+    {
+        $form = $this->getServiceLocator()->get('FormElementManager')
+            ->get(\Omeka\Form\UserForm::class, [
+                'user_id' => $userId,
+                'include_role' => true,
+                'include_admin_roles' => true,
+                'include_is_active' => true,
+                'current_password' => false,
+                'include_password' => false,
+                'include_key' => false,
+            ]);
+        $form->init();
+        return $form
+            ->get('user-settings');
     }
 
     public function viewUserDetails(Event $event)
