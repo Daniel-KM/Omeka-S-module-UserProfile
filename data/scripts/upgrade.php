e06297ebd6353a408b16f02276a85a469b67d3fa
Daniel Berthereau <Daniel.git@Berthereau.net>
2024-02-12 00:00:00 +0000

    Released version 3.4.7.

--- data/scripts/upgrade.php
+++ data/scripts/upgrade.php
@@ -2,6 +2,7 @@
 
 namespace UserProfile;
 
+use Common\Stdlib\PsrMessage;
 use Omeka\Stdlib\Message;
 
 /**
@@ -19,6 +20,7 @@ use Omeka\Stdlib\Message;
 $plugins = $services->get('ControllerPluginManager');
 $api = $plugins->get('api');
 $settings = $services->get('Omeka\Settings');
+$translate = $plugins->get('translate');
 $connection = $services->get('Omeka\Connection');
 $messenger = $plugins->get('messenger');
 $entityManager = $services->get('Omeka\EntityManager');
@@ -30,4 +32,22 @@ if (version_compare($oldVersion, '3.4.4.6', '<')) {
         'It is now possible to remove values from guest form and to hide them in admin form.' // @translate
     );
     $messenger->addSuccess($message);
+}
+
+if (version_compare($oldVersion, '3.4.7', '<')) {
+    if (!method_exists($this, 'checkModuleActiveVersion') || !$this->checkModuleActiveVersion('Common', '3.4.52')) {
+        $message = new Message(
+            $translate('The module %1$s should be upgraded to version %2$s or later.'), // @translate
+            'Common', '3.4.52'
+        );
+        throw new \Omeka\Module\Exception\ModuleCannotInstallException((string) $message);
+    }
+
+    // Update hidden list of fields.
+    if (!$this->updateListFields()) {
+        $message = new PsrMessage(
+            'You should fix the config of the module.' // @translate
+        );
+        $messenger->addWarning($message);
+    }
 }
